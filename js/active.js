775a0871bceb496a145a571f7712a76887688d15
Golam Sarwar <32290268+sarwar12@users.noreply.github.com>
2020-01-30 16:54:25 +0600

    Add files via upload

---
+++ js/active.js
@@ -0,0 +1,11 @@
+$(document).ready(function($){
+	$(".team-carousel").owlCarousel({
+		items:3,
+		margin:30,
+		loop:true,
+		dots:false,
+		nav:true,
+		/* autoplay:true,
+		autoplayTimeout:1000 */
+	});
+});
